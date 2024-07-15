4116d69db81f428bc84cf42619ed10a29a0f4f15
dbarr2 <ddbarr6@gmail.com>
2024-07-15 11:32:39 -0400

    Add authorization-based code for dietary service

---
+++ controlFlow/dietaryService.js
@@ -0,0 +1,29 @@
+let person = "Enrolled Member"
+let accessLevel;
+
+switch(person) {
+    case "Employee":
+    case "Enrolled Member":
+        accessLevel = "Full access"
+        break;
+    case "Subscriber":
+        accessLevel = "Partial"
+        break;
+    default:
+        accessLevel = "Not enrolled in system. Please enroll or subscribe to access this facility";
+}
+
+console.log("User Status: ", person)
+console.log("User Access Level: ", accessLevel)
+
+let dieticianAccess = false;
+switch(person) {
+    case "Enrolled Member":
+        dieticianAccess = true;
+    default:
+        dieticianAccess = false; 
+}
+
+
+console.log("According to our records you are a ",person, "and your access level is ", accessLevel)
+let dieticianString = dieticianAccess == true ? "You have access to one-on-one dietician resources" : "You do not have access to the dietician"
